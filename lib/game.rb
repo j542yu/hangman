1578c9dcf61f1413694e86c9cd19c7bb0fc526c1
judywsl <j542yu@uwaterloo.ca>
2025-01-26 16:29:57 -0500

    Reoganize methods and allow player to save
     - any method that prints text to the command line
       is moved to game_displayable.rb to declutter class
     - player can now choose to save or exit game
       instead of making a guess

--- lib/game.rb
+++ lib/game.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-require_relative 'hangman_constants'
-
 # This class represents a command-line implementation of the classic Hangman game.
 #
 # In this game, the player attempts to guess a hidden word one letter at a time, with a limited
 # number of incorrect guesses allowed.
 class HangmanGame
+  include GameSaveable
   include HangmanConstants
+  include HangmanDisplayableMessages
 
   def initialize
     @word = pick_word.split('')
@@ -19,13 +19,14 @@ class HangmanGame
   attr_reader :word
 
   def play
-    message = give_game_intro
+    message = nil
+    show_game_intro
     until @guessed_word == @word || @num_mistakes == MAX_NUM_MISTAKES
       display_current_status
-      guess = player_make_guess(message)
-      message = give_feedback(guess)
+      input = prompt_player(message)
+      message = give_feedback(input)
     end
-    end_game
+    exit_game
   end
 
   def pick_word
@@ -33,31 +34,48 @@ class HangmanGame
     words.select { |word| word.length.between?(MIN_WORD_LENGTH, MAX_WORD_LENGTH) }.sample
   end
 
-  def player_make_guess(feedback)
-    puts feedback unless feedback.nil?
-    puts "\nThe secret word is #{@word.length} letters long."
-    show_past_guesses unless @past_guesses.empty?
-    puts 'What letter do you want to test?'
-    ask_for_letter_guess
-  end
+  def prompt_player(message)
+    puts message unless message.nil?
 
-  def ask_for_letter_guess # rubocop:disable Metrics/MethodLength
+    show_past_guesses unless @past_guesses.empty?
+
+    show_possible_decisions
+
     loop do
-      print '=> '
-      guess = gets.chomp.downcase
-      if !guess.match?(/[a-z]/)
-        puts "\nInvalid guess... Please guess a letter from a to z!"
-      elsif guess.length != 1
-        puts "\nYou can only guess one letter at a time. Please try again."
-      elsif @past_guesses.include?(guess)
-        puts "\nYou've already tried this letter, it didn't work! Please try again."
-      else
-        return guess
-      end
+      input = ask_for_decision
+      return input if valid_guess?(input)
     end
   end
 
-  def validate_and_update(guess)
+  def ask_for_decision
+    print '=> '
+    gets.chomp.downcase.tap do |input|
+      handle_commands(input)
+    end
+  end
+
+  def handle_commands(input)
+    if input == 'save'
+      save_game
+    elsif input == 'end'
+      exit_game
+    end
+  end
+
+  def valid_guess?(input)
+    if !input.match?(/[a-z]/)
+      warn_not_letter
+    elsif input.length != 1
+      warn_too_long
+    elsif @past_guesses.include?(input)
+      warn_already_tried
+    else
+      return true
+    end
+    false
+  end
+
+  def correct?(guess)
     if @word.include?(guess)
       @guessed_word.each_with_index do |_, index|
         @guessed_word[index] = guess if word[index] == guess
@@ -70,56 +88,13 @@ class HangmanGame
     end
   end
 
-  def give_feedback(guess) # rubocop:disable Metrics/MethodLength
-    result = ''
-
-    got_correct_letter = validate_and_update(guess)
-    if got_correct_letter
-      result = "\nYay! '#{guess}' is in the secret word"
+  def exit_game
+    game_over = @num_mistakes == MAX_NUM_MISTAKES || @guessed_word == @word
+    if game_over
+      display_end_message
     else
-      num_mistakes_left = MAX_NUM_MISTAKES - @num_mistakes
-      result = "\nUh oh... '#{guess}' isn't in the secret word :("
-      result += if num_mistakes_left.zero?
-                  'Your next mistake will kill hangman.'
-                else
-                  "\nYou can make #{num_mistakes_left} more mistake(s) before hangman dies."
-                end
+      display_exit_message
     end
-    result
-  end
-
-  def show_past_guesses
-    puts "\nPast incorrect guesses: '#{@past_guesses.join(', ')}'"
-  end
-
-  def give_game_intro
-    puts 'Welcome to Hangman!'
-    puts 'The computer will generate a secret code, which you have to guess ' \
-    'letter by letter to save hangman.'
-    puts 'On your seventh mistake, he dies.'
-  end
-
-  def end_game
-    if @num_mistakes == MAX_NUM_MISTAKES
-      display_current_status
-      puts 'You failed! Hangman has died.'
-    else
-      puts 'Congratulations!!! You saved hangman.'
-    end
-    puts "The secret word was '#{@word.join('')}'."
-  end
-
-  def display_current_status
-    puts "\n————————————————————————————————————————————————\n"
-    puts "\n#{hangman_drawing(@num_mistakes)}\n\n"
-
-    return if @num_mistakes == MAX_NUM_MISTAKES
-
-    @guessed_word.each do |letter|
-      print "#{letter} "
-    end
-    puts "\n"
+    exit
   end
 end
-
-HangmanGame.new.play

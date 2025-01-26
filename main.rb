6832a59ed5d0cde8f3f7bcf7784976167a457f9d
judywsl <j542yu@uwaterloo.ca>
2025-01-26 16:31:50 -0500

    Add new files in require_relative paths

---
+++ main.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'lib/hangman_constants'
+require_relative 'lib/game_saveable'
+require_relative 'lib/game_displayable'
+require_relative 'lib/game'
+
+HangmanGame.new.play
